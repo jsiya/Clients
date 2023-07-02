5494061053b1ecbad9b9279ad0e95fa77f8dc284
jsiya <jabbarovasia@gmail.com>
2023-07-03 02:26:22 +0400

    Update TextBox1, LoginView and MainViewModel

--- UserControls/TextBox1.xaml.cs
+++ UserControls/TextBox1.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Clients.ViewModels;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -9,26 +7,65 @@ namespace Clients.UserControls
     /// <summary>
     /// Interaction logic for TextBox1.xaml
     /// </summary>
-    public partial class TextBox1 : UserControl, INotifyPropertyChanged
+    public partial class TextBox1 : UserControl
     {
+        //private string _placeholder;
+        //public string Placeholder
+        //{
+        //    get { return _placeholder; }
+        //    set
+        //    {
+        //        if (_placeholder != value)
+        //        {
+        //            _placeholder = value;
+        //            OnPropertyChanged(nameof(Placeholder));
+        //        }
+        //    }
+        //}
+
+        //private string _userInput;
+        //public string UserInput
+        //{
+        //    get { return _userInput; }
+        //    set
+        //    {
+        //        if (_userInput != value)
+        //        {
+        //            _userInput = value;
+        //            OnPropertyChanged(nameof(UserInput));
+        //        }
+        //    }
+        //}
+        public string Placeholder
+        {
+            get { return (string)GetValue(PlaceholderProperty); }
+            set { SetValue(PlaceholderProperty, value); }
+        }
+
+        public static readonly DependencyProperty PlaceholderProperty =
+            DependencyProperty.Register("Placeholder", typeof(string), typeof(TextBox1));
+
+        public string UserInput
+        {
+            get { return (string)GetValue(UserInputProperty); }
+            set { SetValue(UserInputProperty, value); }
+        }
+
+        public static readonly DependencyProperty UserInputProperty =
+            DependencyProperty.Register("UserInput", typeof(string), typeof(TextBox1));
+
+
         public TextBox1()
         {
             InitializeComponent();
-            DataContext = this;
+            //DataContext = this;
         }
 
-        private string _placeholder;
+        public event PropertyChangedEventHandler PropertyChanged;
 
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public string Placeholder
+        protected virtual void OnPropertyChanged(string propertyName)
         {
-            get { return _placeholder; }
-            set 
-            { 
-                _placeholder = value;
-                OnPropertyChanged();
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
@@ -37,9 +74,6 @@ namespace Clients.UserControls
                 tbPlaceholder.Visibility = Visibility.Visible;
             else tbPlaceholder.Visibility = Visibility.Hidden;
         }
-        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+
     }
 }
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,9 +1,15 @@
 ﻿using Clients.Commands;
 using Clients.DB;
 using Clients.Models;
+using Clients.UserControls;
+using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 
 namespace Clients.ViewModels;
@@ -11,28 +17,30 @@ namespace Clients.ViewModels;
 public class MainViewModel : INotifyPropertyChanged
 {
     public ObservableCollection<User> Users { get; set; }
-    private string _username;
+    private TextBox1 _textboxUsername;
 
-    public string Username
+    public TextBox1 TextBoxUsername
     {
-        get { return _username; }
+        get { return _textboxUsername; }
         set 
         { 
-            _username = value; 
+            _textboxUsername = value;
             OnPropertyChanged();
         }
     }
-    private string _password;
-    public string Password
+
+    private TextBox1 _textboxPassword;
+
+    public TextBox1 TextBoxPassword
     {
-        get { return _password; }
+        get { return _textboxPassword; }
         set
         {
-            _password = value;
+            _textboxPassword = value;
             OnPropertyChanged();
         }
     }
-    public ICommand LoginCommand { get; set; }
+    public ICommand? LoginCommand { get; set; }
 
     public UsersDB usersDB;
 
@@ -43,30 +51,37 @@ public class MainViewModel : INotifyPropertyChanged
     { 
         usersDB = new UsersDB();
         Users = usersDB.Users;
+        TextBoxUsername = new TextBox1();
+        TextBoxUsername.Placeholder = "please enter username";
+        TextBoxUsername.UserInput = "";
+        TextBoxPassword = new TextBox1();
+        TextBoxPassword.Placeholder = "please enter password";
+        TextBoxPassword.UserInput = "";
         LoginCommand = new RelayCommand(LogIn, checkUser);
     }
 
     public bool checkUser(object? parametr)
     {
-        foreach (User user in Users)
-        {
-            if(user.Username == Username &&  user.Password == Password)
-            {
-                return true;
-            }
-        }
-        return false;
+        if (string.IsNullOrEmpty(TextBoxUsername.UserInput) || string.IsNullOrEmpty(TextBoxPassword.UserInput)) return false;
+        return true;
     }
 
     public void LogIn(object? parametr)
     {
         foreach (User user in Users)
         {
-            if (user.Username == Username && user.Password == Password)
+            if (user.Username == TextBoxUsername.UserInput && user.Password == TextBoxPassword.UserInput)
             {
                 SelectedUser = user;
             }
         }
+        //JsonSerializerOptions options = new JsonSerializerOptions();
+        //options.WriteIndented = true;
+        //if (Users != null)
+        //{
+        //    string folderPath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "SourceDataFiles", "Orders.json");
+        //    File.WriteAllText(folderPath, JsonSerializer.Serialize(Users, options));
+        //}
     }
     private void OnPropertyChanged([CallerMemberName]  string? propertyName = null)
     {
